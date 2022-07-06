707d6559116e35f23d5fedd4d4b4357bf7ac7593
ThomazAquino <thomazmanoel.aquino@gmail.com>
2022-07-06 15:08:01 -0300

    foundations

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post';
+
+export function App() {
+
+  return (
+    <div>
+      <Post
+        author="Thomaz D'Aquino"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum animi, tenetur aliquam accusantium tempore in iste aut quisquam consequuntur quaerat sequi! Accusamus, harum quo architecto ex exercitationem distinctio beatae eveniet."
+      />
+      <Post
+        author="Thomaz D'Aquino2 "
+        content="2 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum animi, tenetur aliquam accusantium tempore in iste aut quisquam consequuntur quaerat sequi! Accusamus, harum quo architecto ex exercitationem distinctio beatae eveniet."
+      />
+    </div>
+  )
+}
+
