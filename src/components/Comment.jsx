8aedad41cbc9d5a1635b3af88356ce9519c9c197
ThomazAquino <thomazmanoel.aquino@gmail.com>
2022-07-10 14:13:49 -0300

    feat(comments): implement like count

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,32 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    // If we update the state like this
+    // the like count will not increment by 3
+    // because the way of React treat closures.
+    // setLikeCount(likeCount + 1);
+    // setLikeCount(likeCount + 1);
+    // setLikeCount(likeCount + 1);
+
+    // When we update the sate with arrow func,
+    // The parameter of the function have the 
+    // latest value of the state. This can be
+    // useful when we need the fresh value of
+    // the state.
+    setLikeCount((state) => {
+      return state + 1;
+    })
+
   }
 
   return (
@@ -18,7 +40,7 @@ export function Comment({ content, onDeleteComment }) {
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Leonardo Brizolla</strong>
+              <strong>Thomaz D'Aquino</strong>
               <time 
                 title="08 de junho às 11:31" 
                 dateTime="2022-06-08 11:31:00"
@@ -40,9 +62,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
